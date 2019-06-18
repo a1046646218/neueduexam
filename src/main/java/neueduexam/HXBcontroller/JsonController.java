dfcfd39e5e4a86908aa5c83d1da9bc103ee8f71a
ruojichong <d00000.0@qq.com>
2019-06-18 15:27:21 +0800

    完成了查看考试邀请表界面

--- src/main/java/neueduexam/HXBcontroller/JsonController.java
+++ src/main/java/neueduexam/HXBcontroller/JsonController.java
@@ -1,13 +1,28 @@
 package neueduexam.HXBcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import neueduexam.HXBservice.InvitationService;
+import neueduexam.HXBservice.TestPaperService;
 
 @RestController
 public class JsonController {
 	
+	@Autowired
+	InvitationService invitationService;
+	
+
+	
 	@RequestMapping("/invitationJson")
 	public String getInvitationJson(String testId) {
+		String json = invitationService.selectInvitationByTestId("1");
+		return json;
+	}
+	
+	@RequestMapping("/getTestName")
+	public String getTestName(String testId) {
 		
 		return null;
 	}
