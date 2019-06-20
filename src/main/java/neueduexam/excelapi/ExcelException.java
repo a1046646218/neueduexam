d50a941dc6faef34241dbc390ea3eb28d22490b5
a1046646218 <38356038+a1046646218@users.noreply.github.com>
2019-06-20 09:59:11 +0800

    excel的api导入

---
+++ src/main/java/neueduexam/excelapi/ExcelException.java
@@ -0,0 +1,11 @@
+package neueduexam.excelapi;
+/**
+ * 捕获相关 Exception
+ * @author lgp
+ *
+ */
+public class ExcelException extends RuntimeException {
+    public ExcelException(String message) {
+        super(message);
+    }
+}
